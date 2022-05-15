85504ceb1578c0f4904119f2181d78d9c72505df
theATM <the_atm@wp.pl>
2022-05-15 22:47:17 +0200

    Training loop cnd

--- LipenNet/Code/Functional/training.py
+++ LipenNet/Code/Functional/training.py
@@ -1,7 +1,10 @@
 import sys
+import time
+
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim import lr_scheduler
+from torch.nn.utils import clip_grad_norm_
 
 import Code.Profile.profileloader as pl
 import Code.Dataloader.lipenset as dl
@@ -31,8 +34,58 @@ def main():
         print("Learning rate = %1.5f" % scheduler.get_last_lr().pop())
         #Train one epoch
         model.train()
-        
+        multi_batch_loss = 0.0
+        grad_per_batch_const = 8 #TODO
+        for i, data in enumerate(train_loader):
+            inputs = torch.autograd.Variable(data['image'].to(device, non_blocking=True))
+            labels = torch.autograd.Variable(data['label'].to(device, non_blocking=True))
+            with torch.set_grad_enabled(True):
+                # Calculate Network Function (what Network thinks of this image)
+                output = model(inputs)
+                # Calculate loss
+                loss = criterion(output, labels)
+                # Backpropagate loss
+                loss.backward()
+                multi_batch_loss += loss;
+                if ((i + 1) % multi_batch_loss) == 0:
+                    # Normalize loss to account for batch accumulation
+                    multi_batch_loss = multi_batch_loss / multi_batch_loss
+                    # Clipping the gradient
+                    clip_grad_norm_(model.parameters(), max_norm=1)
+                    # Update the weighs
+                    optimizer.step()
+                    # Resets Gradient to Zeros (clearing it before using it next time in calculations)
+                    optimizer.zero_grad()
+                    # Resets multi batch loss sum
+                    multi_batch_loss = 0.0
+                # Calculate Accuracy
+                # Update Statistics
+                # TODO
+        # Print Results per epoch
+        # TODO
+        # Stepping scheduler
+        scheduler.step()
 
+        # Evaluate in some epochs:
+        epoch_per_eval_const = 10 #TODO
+        if (epoch + 1) % epoch_per_eval_const == 0:
+            model.eval()
+            evaluation_time = time.perf_counter()
+            # Evaluate
+            # TODO
+            # Save Model Checkpoint
+            # TODO
+        print('Epoch ' + str(epoch + 1) + ' completed')
+
+    print("\nTraining concluded\n")
+    model.eval()
+    # Post Training Evaluation
+    # TODO
+    # Print Stats
+    # TODO
+    # Save Model
+    # TODO
+    print("Bye")
 
 
 
